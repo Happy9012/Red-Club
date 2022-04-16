6d11564fc70c3fe7da2c55eaef5e83c74fdcc117
ARUN NOOB <83957038+arunsingh-creator@users.noreply.github.com>
2022-04-16 15:01:45 +0530

    Update update.py

--- update.py
+++ update.py
@@ -42,7 +42,7 @@ try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_BRANCH = 'main'
+    UPSTREAM_BRANCH = 'master'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
